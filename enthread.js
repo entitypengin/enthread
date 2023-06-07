d34f8de7b5cd6f61a49c9a0fac169f8fb0b1ab02
EntityPENGIN <nopedoritos@gmail.com>
2023-06-07 15:13:10 +0900

    github

--- enthread.js
+++ enthread.js
@@ -29,7 +29,23 @@ if (searchParams.has("x")) {
 
 document.title = "Enthread-Beta";
 $("body").empty();
-$("body").append('<h1><p class="title"><a class="top" href="./">EnthreadBeta</a></p></h1><h2><div id="send"><textarea id="send_author"></textarea><textarea id="send_message"></textarea><button id="send_button">SEND</button><hr></div><div id="texts"></div></h2>');
+$("body").append(`<h1>
+    <p class="title">
+        <a class="top" href="./">EnthreadBeta</a>
+    </p>
+</h1>
+<h2>
+    <div id="send">
+        <textarea id="send_author"></textarea>
+        <textarea id="send_message"></textarea>
+        <button id="send_button">SEND</button>
+        <hr>
+    </div>
+    <div id="texts"></div>
+</h2>
+<h2>
+    <a href="https://github.com/entitypengin/enthread">Github</a>
+</h2>`);
 
 $("#send_button").on("click", function () {
     const author = document.getElementById("send_author").value;
@@ -50,7 +66,13 @@ function setTexts(texts) {
     for (var id in texts) {
         text = texts[id].message.replace("<", "&lt;").replace(">", "&gt;");
         time = new Date(texts[id].timestamp).toISOString();
-        $("#texts").prepend(`<div id="x${i}" class="text"><div class="content"><p class="id">${i}: ${texts[id].author} (${time})</p><p class="message">${text}</p></div><hr></div>`);
+        $("#texts").prepend(`<div id="x${i}" class="text">
+    <div class="content">
+        <p class="id">${i}: ${texts[id].author} (${time})</p>
+        <p class="message">${text}</p>
+    </div>
+    <hr>
+</div>`);
         i++;
     }
 }
