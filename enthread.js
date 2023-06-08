58cb7c156bf2dafd286c9a2a6266c9ff6e67950d
EntityPENGIN <nopedoritos@gmail.com>
2023-06-08 16:52:57 +0900

    host

--- enthread.js
+++ enthread.js
@@ -71,7 +71,9 @@ function setTexts(texts) {
         }).replace(/#\d*/g, (str) => {
             return `<a href="./?x=${str.slice(1)}">${str}</a>`;
         });
-        host = texts[id].host;
+        host = texts[id].host.replace(/([a-zA-Z]+:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?/ig, (str) => {
+            return `<a href="${str}">${str}</a>`;
+        });
         time = new Date(texts[id].timestamp).toISOString();
         $("#texts").prepend(`<div id="x${i}" class="text">
     <div class="content">
