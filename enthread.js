835e32c5b017a044be031164e37435e26c2248ca
EntityPENGIN <nopedoritos@gmail.com>
2023-06-10 11:52:02 +0900

    link

--- enthread.js
+++ enthread.js
@@ -27,9 +27,19 @@ if (searchParams.has("x")) {
     textParam = searchParams.get("x");
 }
 
+function timeFormat(date) {
+    return `${("0000" + date.getUTCFullYear()).slice(-4)}-${("00" + date.getUTCMonth()).slice(-2)}-${("00" + date.getUTCDate()).slice(-2)} ${("00" + date.getUTCHours()).slice(-2)}:${("00" + date.getUTCMinutes()).slice(-2)}:${("00" + date.getUTCSeconds()).slice(-2)}`;
+}
+
+function replaceToLink(str) {
+    return str.replace(/([a-zA-Z]+:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?/ig, (str) => {
+        return `<a href="https://${str}">${str}</a>`;
+    });
+}
+
 document.title = "Enthread-Beta";
 $("body").empty();
-$("body").append(`<h1><p class="title"><a class="top" href="${location.pathname}">EnthreadBeta</a></p></h1><h2><hr><div id="send" class="text"><p class="id"><span id="length">0</span>: <input type="text" id="send_author" placeholder="Your name">(${location.hostname}, <span id="time">2038-01-19 03:14:07</span>)</p><p class="message"><textarea id="send_message" placeholder="Your message"></textarea></p><p id="button"><input type="button" id="send_button" value="SEND"></p><hr></div><div id="texts"></div><div><a href="https://github.com/entitypengin/enthread">Github</a></div></h2>`);
+$("body").append(`<h1><p class="title"><a class="top" href="${location.pathname}">EnthreadBeta</a></p></h1><h2><hr><div id="send" class="text"><p class="id"><span id="length">0</span>: <input type="text" id="send_author" placeholder="Your name">(${replaceToLink(location.hostname)}, <span id="time">2038-01-19 03:14:07</span>)</p><p class="message"><textarea id="send_message" placeholder="Your message"></textarea></p><p id="button"><input type="button" id="send_button" value="SEND"></p><hr></div><div id="texts"></div><div><a href="https://github.com/entitypengin/enthread">Github</a></div></h2>`);
 
 $("#send_button").on("click", function () {
     const newTextRef = push(textsRef)
@@ -40,10 +50,6 @@ $("#send_button").on("click", function () {
         timestamp: Date.now()
     });
 });
-
-function timeFormat(date) {
-    return `${("0000" + date.getUTCFullYear()).slice(-4)}-${("00" + date.getUTCMonth()).slice(-2)}-${("00" + date.getUTCDate()).slice(-2)} ${("00" + date.getUTCHours()).slice(-2)}:${("00" + date.getUTCMinutes()).slice(-2)}:${("00" + date.getUTCSeconds()).slice(-2)}`;
-}
 
 setInterval(() => {
     $("#time").text(timeFormat(new Date()));
@@ -63,9 +69,7 @@ function setTexts(texts) {
         }).replace(/#\d+/g, (str) => {
             return `<a href="?x=${str.slice(1)}">${str}</a>`;
         });
-        host = `${texts[id].host}`.replace(/([a-zA-Z]+:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?/ig, (str) => {
-            return `<a href="https://${str}">${str}</a>`;
-        });
+        host = replaceToLink(texts[id].host);
         time = timeFormat(new Date(texts[id].timestamp));
         $("#texts").prepend(`<div id="x${i}" class="text"><div class="content"><p class="id">${i}: ${author} (${host}, ${time})</p><p class="message">${message}</p></div><hr></div>`);
         i++;
