701c9e3f2b494b64acf22dd0aa7c66a09aee8813
EntityPENGIN <nopedoritos@gmail.com>
2023-07-06 21:19:29 +0900

    document.getElementById

--- enthread.js
+++ enthread.js
@@ -35,37 +35,18 @@ function replaceToLink(str) {
     return str.replace(/([a-zA-Z]+:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?/ig, str => `<a href="https://${str}">${str}</a>`);
 }
 
-function ImageToBase64(img, mime_type) {
-    console.log(typeof(img));
-
-    var canvas = document.createElement('canvas');
-    canvas.width  = img.width;
-    canvas.height = img.height;
-
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-
-    return canvas.toDataURL(mime_type);
-}
-
-function sendText(author, message, file=null, mime_type=null) {
-    // var encoded = "";
-    // if (file !== null && mime_type !== null) {
-    //     console.log(typeof(file));
-    //     encoded = `!!${mime_type}:${ImageToBase64(file, mime_type)}`
-    // }
-
+function sendText(author, message) {
     const newTextRef = push(textsRef);
     set(newTextRef, {
         author: author,
-        message: message /* + encoded */,
+        message: message,
         host: location.hostname,
         timestamp: Date.now()
     });
 }
 
 function setTexts(texts) {
-    $("#texts").empty();
+    document.getElementById("texts").empty();
     var i = 0;
     var author = "";
     var message = "";
@@ -79,17 +60,17 @@ function setTexts(texts) {
         if (message == "!!l") {
             message = "Show...";
         }
-        $("#texts").prepend(`<div id="x${i}" class="text"><div class="content"><p class="id">${i}: ${author} (${host}, ${time})</p><p class="message">${message}</p></div><hr noshade></div>`);
+        document.getElementById("texts").prepend(`<div id="x${i}" class="text"><div class="content"><p class="id">${i}: ${author} (${host}, ${time})</p><p class="message">${message}</p></div><hr noshade></div>`);
         i++;
     }
-    $("#length").text(`${i}`);
+    document.getElementById("length").text(`${i}`);
 }
 
 document.title = "Enthread-Beta";
 $("body").empty();
 $("body").append(`<h1><p class="title"><a class="top" href="${location.pathname}">EnthreadBeta</a></p></h1><h2><hr noshade><div id="send" class="text"><p class="id"><span id="length">0</span>: <input type="text" id="send_author" placeholder="Your name">(${replaceToLink(location.hostname)}, <span id="time">2038-01-19 03:14:07</span>)</p><div class="areas"><div><textarea id="send_message" placeholder="Your message"></textarea></div></div><div class="buttons flex-box-between"><div class="button"><input type="file" id="send_file"></div><div class="button"><input type="button" id="send_button" value="SEND"></div></div><hr noshade></div><div id="texts"></div><div><a href="https://github.com/entitypengin/enthread">Github</a></div></h2>`);
 
-$("#send_button").on("click", () => {
+document.getElementById("send_button").on("click", () => {
     var file = document.getElementById("send_file");
     var mime_type = null;
     if (file.files.length != 0) {
@@ -98,7 +79,7 @@ $("#send_button").on("click", () => {
     sendText(document.getElementById("send_author").value, document.getElementById("send_message").value, file, "image/jpeg");
 });
 
-setInterval(() => $("#time").text(timeFormat(new Date())), 1000);
+setInterval(() => document.getElementById("time").text(timeFormat(new Date())), 1000);
 
 get(textsRef).then(snapshot => {
     if (snapshot.exists()) {
@@ -106,7 +87,7 @@ get(textsRef).then(snapshot => {
         try {
             if (textParam != "") {
                 var animeSpeed = 500;
-                var target = $(`#x${textParam}`);
+                var target = document.getElementById(`x${textParam}`);
                 var position;
                 position = target.offset().top;
                 $("body,html").stop().animate({
