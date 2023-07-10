078c7e639c52382d038dc74c3f3dd82f9b5fd4f2
EntityPENGIN <nopedoritos@gmail.com>
2023-07-10 19:24:55 +0900

    e

--- enthread.js
+++ enthread.js
@@ -79,6 +79,7 @@ function setTexts(ids) {
                 }
                 $("#texts").append(`<div id="x${i}" class="text"><div class="content"><p class="id">${i}: ${author} (${host}, ${time})</p><p class="message", id="message_x${i}">${message}</p></div><hr noshade></div>`);
                 $(`#button_x${i}`).on("click", {html_id: i, message_id: keys[i]}, openText);
+                console.log({html_id: i, message_id: keys[i]});
             }
         }).catch(error => console.error(error));
     }
