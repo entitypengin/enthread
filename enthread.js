81745136ab6989bcedcef26bf1cd93a19a7ff707
EntityPENGIN <nopedoritos@gmail.com>
2023-07-10 18:23:32 +0900

    reverse

--- enthread.js
+++ enthread.js
@@ -66,7 +66,7 @@ function setTexts(ids) {
     var message;
     var host;
     var time;
-    for (var id in ids) {
+    for (var id in Object.keys(ids).reverse()) {
         get(ref(database, `texts/${id}`)).then(snapshot => {
             i--;
             if (snapshot.exists()) {
