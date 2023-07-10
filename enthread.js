e583c238ed72e9567156f75005cc0a1ffa85c907
EntityPENGIN <nopedoritos@gmail.com>
2023-07-10 20:54:05 +0900

    time

--- enthread.js
+++ enthread.js
@@ -26,7 +26,7 @@ if (searchParams.has("t")) {
 const threadRef = ref(database, `threads/${threadParam}/texts`);
 
 function formatTime(date) {
-    return `${("0000" + date.getUTCFullYear()).slice(-4)}-${("00" + date.getUTCMonth()).slice(-2)}-${("00" + date.getUTCDate()).slice(-2)} ${("00" + date.getUTCHours()).slice(-2)}:${("00" + date.getUTCMinutes()).slice(-2)}:${("00" + date.getUTCSeconds()).slice(-2)}`;
+    return `${("0000" + date.getUTCFullYear()).slice(-4)}-${("00" + (date.getUTCMonth() + 1)).slice(-2)}-${("00" + date.getUTCDate()).slice(-2)} ${("00" + date.getUTCHours()).slice(-2)}:${("00" + date.getUTCMinutes()).slice(-2)}:${("00" + date.getUTCSeconds()).slice(-2)}`;
 }
 
 function replaceToLink(str) {
