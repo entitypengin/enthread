4131adda1abc0d3a17f1a5b25e6240c993437e2d
EntityPENGIN <nopedoritos@gmail.com>
2023-07-14 16:36:08 +0900

    css

--- enthread.js
+++ enthread.js
@@ -10,83 +10,6 @@ import {
     update
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
-const css_presets = {
-    normal: "",
-    white: `
-hr {
-    color: #AAA;
-    border-color: #AAA;
-}
-a:link {
-    color: #0A0;
-}
-a:hover {
-    color: #0AA;
-}
-a:visited {
-    color: #A0A;
-}
-a:active {
-    color: #AA0;
-}
-a.top:link,a.top:visited {
-    color: black;
-}
-a.top:hover {
-    color: #AAA;
-}
-a.top:active {
-    color: #555;
-}
-body {
-    background-color: white;
-    color: black;
-}
-textarea {
-    border-color: #AAA;
-    background-color: white;
-    color: black;
-}
-::placeholder {
-    color: #AAA;
-}
-textarea:hover {
-    border-color: #0AA;
-}
-textarea:focus {
-    border-color: #AA0;
-}
-input {
-    background-color: white;
-    color: black;
-}
-::placeholder {
-    color: #AAA;
-}
-input[type="text"] {
-    border-bottom-color: #AAA;
-}
-input[type="text"]:hover {
-    border-bottom-color: #0AA;
-}
-input[type="text"]:focus {
-    border-bottom-color: #AA0;
-}
-input[type="button"] {
-    color: #AAA;
-    border-bottom-color: #AAA;
-}
-input[type="button"]:hover {
-    color: #0AA;
-    border-bottom-color: #0AA;
-}
-input[type="button"]:active {
-    color: #AA0;
-    border-bottom-color: #AA0;
-}
-`
-}
-
 function formatTime(date) {
     return `${("0000" + date.getUTCFullYear()).slice(-4)}-${("00" + (date.getUTCMonth() + 1)).slice(-2)}-${("00" + date.getUTCDate()).slice(-2)} ${("00" + date.getUTCHours()).slice(-2)}:${("00" + date.getUTCMinutes()).slice(-2)}:${("00" + date.getUTCSeconds()).slice(-2)}`;
 }
@@ -173,7 +96,6 @@ if (searchParams.has("t")) {
 }
 
 if (threadParam !== null) {
-    $("head").append("<style id='stylesheet' type='text/css'></style>");
     $("body").empty();
     $("body").append(`<h1><p class="title"><a class="top" href="${location.origin}${location.pathname}">Enthread</a></p><p id="thread_name"></p></h1><h2><hr noshade><div id="send" class="text"><p class="id"><span id="length">0</span>: <input type="text" id="send_author" placeholder="Your name">(${replaceToLink(location.hostname)}, <span id="time">2038-01-19 03:14:07</span>)</p><div class="areas"><div><textarea id="send_message" placeholder="Your message"></textarea></div></div><div class="buttons flex-box-between"><div class="button"><!-- <input type="file" id="send_file"> --></div><div class="button"><input type="button" id="send_button" value="SEND"></div></div><hr noshade></div><div id="texts"></div><div><a href="https://github.com/entitypengin/enthread">Github</a></div></h2>`);
 
@@ -181,7 +103,7 @@ if (threadParam !== null) {
         const thread = snapshot.val();
         document.title = `${thread.name} - Enthread`;
         $("#thread_name").text(thread.name);
-        $("#stylesheet").html(css_presets[thread.css_presets]);
+        $("head").append(`<link type="text/css" rel="stylesheet" href="https://entitypengin.github.io/enthread/css/${thread.css_presets}.css">`);
     });
 
 
